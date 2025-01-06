b1f90276403da9e55a24c0e3d0ef2e5b49edc766
Abhinn Goyal <158797889+abhinn05@users.noreply.github.com>
2025-01-06 22:29:14 +0530

    Update plot.py

--- Assignments/Assignment_2/2/plot.py
+++ Assignments/Assignment_2/2/plot.py
@@ -2,7 +2,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv(r'D:\\College\\Sem 3\\Design of Algorithm\\2024\\Assignments\\Assignment_2\\2\\output\\sorting_performance_2.csv')
+df = pd.read_csv('sorting_performance_2.csv')
 plt.figure(figsize=(10, 6))
 
 colors = ['brown', 'green', 'purple', 'pink', 'm', 'y', 'k']
